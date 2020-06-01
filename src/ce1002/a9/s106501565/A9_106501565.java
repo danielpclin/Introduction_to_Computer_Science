48773efbfb403d8b7c32553119ff550f515b10ca
danielpclin <daniel.pclin@gmail.com>
2020-06-02 04:03:34 +0800

    add Assignment 9

---
+++ src/ce1002/a9/s106501565/A9_106501565.java
@@ -0,0 +1,28 @@
+/*
+ * Assignment 8
+ * Course: CE1002
+ * Name: 林柏臣
+ * Student ID: 106501565
+ */
+package ce1002.a9.s106501565;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class A9_106501565 {
+    public static void main(String[] args){
+        // Create a Scanner with a buffered System.in InputStreamReader
+        Scanner scanner = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+        int[] heights = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int max = 0;
+        for (int i = 0; i < heights.length; i++) {
+            for (int j = i + 1; j < heights.length; j++) {
+                max = Math.max(Math.min(heights[i], heights[j]) * (j-i), max);
+            }
+        }
+        System.out.println(max);
+    }
+}
